21808bff1715cc29f0c79dace7af9dbe8f4d9fd4
Lidux <36154195+JoanBarduena@users.noreply.github.com>
2023-09-25 12:34:43 +0200

    Project Filter + Animation :100:

---
+++ js/filter.js
@@ -0,0 +1,32 @@
+const triggers = document.querySelectorAll('li.filter-trigger');
+const projects = document.querySelectorAll('.project');
+var all = document.querySelector('.reset');
+
+function clearActive() {
+    var activeLink = document.querySelector('.active');
+    activeLink.classList.remove("active");
+}
+
+triggers.forEach(element => {
+    element.addEventListener('click', function () {
+        clearActive();
+        element.classList.add('active');
+
+        let filter = element.dataset.filter;
+        //console.log(filter);
+
+        projects.forEach(projects => {
+            if (!projects.classList.contains(filter)) {
+                projects.classList.add('hide');
+            } else {
+                projects.classList.remove('hide');
+            }
+        });
+
+        if (filter === 'all') {
+            projects.forEach(projects => {
+                projects.classList.remove('hide');
+            })
+        }
+    })
+});
