cff8abe7cb2b87c4e3df6ff7e32c9ee410bf5061
Lidux <36154195+JoanBarduena@users.noreply.github.com>
2023-09-26 18:00:14 +0200

    Year filter

---
+++ js/year.js
@@ -0,0 +1,55 @@
+
+const years = [];
+var yearsDiv = document.querySelector('.years');
+
+function displayYears() {
+
+    const yearElement = document.querySelectorAll('.year-number');
+
+    yearElement.forEach(function (yearElement) {
+        const parentElement = yearElement.closest('.hide');
+
+        if (!parentElement) {
+            const yearText = yearElement.textContent;
+            const yearNumber = parseInt(yearText, 10);
+            years.push(yearNumber);
+        }
+
+    })
+
+    if (years.length > 0) {
+        let lowerValue = years[0];
+        let higherValue = years[0];
+
+        for (let i = 1; i < years.length; i++) {
+            if (years[i] < lowerValue) {
+                lowerValue = years[i];
+            }
+            if (years[i] > higherValue) {
+                higherValue = years[i];
+            }
+        }
+
+        var contentToInsert = '';
+
+        if (lowerValue == higherValue) {
+            contentToInsert = lowerValue;
+        } else {
+            contentToInsert = lowerValue + " - " + higherValue;
+        }
+
+        var textNode = document.createTextNode(contentToInsert);
+        yearsDiv.appendChild(textNode);
+    }
+
+    console.log(years.length);
+}
+
+function resetYears() {
+    years.length = 0;
+    yearsDiv.innerHTML = '';
+    displayYears();
+}
+
+
+
